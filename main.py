ee204e755d536c66f4faaf2e811f75cb894ff89d
Antonin LYAËT <syukodev@outlook.com>
2026-01-14 13:14:33 +0100

    Remove unsed main

--- main.py
+++
@@ -1,8 +0,0 @@
-"""Entry point for OMEGA - runs script.main"""
-from script.main import main
-import asyncio
-import nest_asyncio
-
-if __name__ == "__main__":
-    nest_asyncio.apply()
-    asyncio.run(main())
